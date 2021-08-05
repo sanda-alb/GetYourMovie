a0e4b0840572879cba3558b6d4aeaab7a2d90efd
albrecht <sanda.albrecht@gmail.com>
2021-08-05 12:32:45 +0300

    NetworkState.kt, MovieDetails added

--- app/src/main/java/com/example/getyourmovie/single_movie_detail/SingleMovie.kt
+++ app/src/main/java/com/example/getyourmovie/single_movie_detail/SingleMovie.kt
@@ -7,6 +7,6 @@ import com.example.getyourmovie.R
 class SingleMovie2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_single_movie2)
+        setContentView(R.layout.activity_single_movie)
     }
 }
